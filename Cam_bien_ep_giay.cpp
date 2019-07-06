e5aa9ff08c3eb8ce201baee0944c84cd7ad3f2f3
Lê Thanh Tuấn <vjpnho@gmail.com>
2019-07-06 12:17:49 +0700

    Cảm biến giấy ép OCTA - Paper Sensor with led tower
    Cảm biến giấy ép OCTA Paper Sensor with led tower

---
+++ Cam_bien_ep_giay.cpp
@@ -0,0 +1,110 @@
+#line 1 "Cam_bien_ep_giay.ino"
+#include "Arduino.h"
+void setup();
+void loop();
+#line 1
+int cambien1 = 5;
+int cambien2 = 6;
+int cambien3 = 7;
+int cambien4 = 8;
+int xanh = 9;
+int ddo = 10;
+int vang = 11;
+
+void setup() {
+  pinMode(cambien1, INPUT);
+  pinMode(cambien2, INPUT);
+  pinMode(cambien3, INPUT);
+  pinMode(cambien4, INPUT);
+  pinMode(ddo, OUTPUT);
+  pinMode(vang, OUTPUT);
+  pinMode(xanh, OUTPUT);
+} 
+void loop() {
+  int GT1 = digitalRead(cambien1); 
+  int GT2 = digitalRead(cambien2);
+  int GT3 = digitalRead(cambien3);
+  int GT4 = digitalRead(cambien4);  
+  
+  if ((GT1 == LOW) && (GT2 == LOW) && (GT3 == LOW) && (GT4 == LOW)) {     // den xanh
+    digitalWrite(xanh, HIGH);
+  } else {
+    digitalWrite(xanh, LOW);                                                       
+  }
+  if ((GT1 == HIGH) && (GT2 == HIGH) && (GT3 == HIGH) && (GT4 == HIGH)) {        // den ddo
+    digitalWrite(ddo, HIGH);
+  } else {
+    digitalWrite(ddo, LOW);                                                         
+  } 
+  if ((GT1 == LOW) && (GT2 == HIGH) && (GT3 == HIGH) && (GT4 == HIGH)) {      // den vang 1
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == LOW) && (GT3 == HIGH) && (GT4 == HIGH)) {      // den vang 2
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == HIGH) && (GT3 == LOW) && (GT4 == HIGH)) {      // den vang 3
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == HIGH) && (GT3 == HIGH) && (GT4 == LOW)) {      // den vang 4
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == LOW) && (GT3 == HIGH) && (GT4 == HIGH)) {      // den vang 5
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == HIGH) && (GT3 == LOW) && (GT4 == LOW)) {      // den vang 6
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == HIGH) && (GT3 == LOW) && (GT4 == HIGH)) {      // den vang 7
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == LOW) && (GT3 == HIGH) && (GT4 == LOW)) {      // den vang 8
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == HIGH) && (GT3 == HIGH) && (GT4 == LOW)) {      // den vang 9
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == LOW) && (GT3 == LOW) && (GT4 == HIGH)) {      // den vang 10
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == LOW) && (GT3 == LOW) && (GT4 == HIGH)) {      // den vang 11
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == LOW) && (GT3 == HIGH) && (GT4 == LOW)) {      // den vang 12
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == LOW) && (GT2 == HIGH) && (GT3 == LOW) && (GT4 == LOW)) {      // den vang 13
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) && (GT2 == LOW) && (GT3 == LOW) && (GT4 == LOW)) {      // den vang 14
+    digitalWrite(vang, HIGH);
+  } else {
+    digitalWrite(vang, LOW);                                                         
+  }
+}
+
---
+++ Cam_bien_ep_giay_Octa.ino
@@ -0,0 +1,108 @@
+int cambien1 = 5;
+int cambien2 = 6;
+int cambien3 = 7;
+int cambien4 = 8;
+int denxanh = 9;
+int dendo = 10;
+int denvang = 11;
+
+void setup() {
+  Serial.begin(9600);   // bat cong serial
+  pinMode(cambien1, INPUT_PULLUP);
+  pinMode(cambien2, INPUT_PULLUP);
+  pinMode(cambien3, INPUT_PULLUP);
+  pinMode(cambien4, INPUT_PULLUP);
+  pinMode(dendo, OUTPUT);
+  pinMode(denvang, OUTPUT);
+  pinMode(denxanh, OUTPUT);
+}
+ 
+void loop() {
+  int GT1 = digitalRead(cambien1); 
+  int GT2 = digitalRead(cambien2);
+  int GT3 = digitalRead(cambien3);
+  int GT4 = digitalRead(cambien4);  
+  Serial.println(GT1); 
+  delay(1000);
+  
+  if ((GT1 == HIGH) && (GT2 == HIGH) && (GT3 == HIGH) && (GT4 == HIGH)) {     // den xanh
+    digitalWrite(denxanh, HIGH);
+  } else {
+    digitalWrite(denxanh, LOW);                                                       
+  }
+  if ((GT1 == LOW) && (GT2 == LOW) && (GT3 == LOW) && (GT4 == LOW)) {        // den do
+    digitalWrite(dendo, HIGH);
+  } else {
+    digitalWrite(dendo, LOW);                                                         
+  } 
+   if ((GT1 == LOW) ^ (GT2 == HIGH) ^ (GT3 == HIGH) ^ (GT4 == HIGH)) {      // den vang 1
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == LOW) ^ (GT3 == HIGH) ^ (GT4 == HIGH)) {      // den vang 2
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == HIGH) ^ (GT3 == LOW) ^ (GT4 == HIGH)) {      // den vang 3
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == HIGH) ^ (GT3 == HIGH) ^ (GT4 == LOW)) {      // den vang 4
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == LOW) ^ (GT3 == HIGH) ^ (GT4 == HIGH)) {      // den vang 5
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == HIGH) ^ (GT3 == LOW) ^ (GT4 == HIGH)) {      // den vang 6
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == HIGH) ^ (GT3 == HIGH) ^ (GT4 == LOW)) {      // den vang 7
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == LOW) ^ (GT3 == LOW) ^ (GT4 == HIGH)) {      // den vang 8
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == LOW) ^ (GT3 == HIGH) ^ (GT4 == LOW)) {      // den vang 9 
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == HIGH) ^ (GT3 == LOW) ^ (GT4 == LOW)) {      // den vang 10
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == HIGH) ^ (GT2 == LOW) ^ (GT3 == LOW) ^ (GT4 == LOW)) {      // den vang 11
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == HIGH) ^ (GT3 == LOW) ^ (GT4 == LOW)) {      // den vang 12
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == LOW) ^ (GT3 == HIGH) ^ (GT4 == LOW)) {      // den vang 13
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+  if ((GT1 == LOW) ^ (GT2 == LOW) ^ (GT3 == LOW) ^ (GT4 == HIGH)) {      // den vang 14
+    digitalWrite(denvang, HIGH);
+  } else {
+    digitalWrite(denvang, LOW);                                                         
+  }
+}
